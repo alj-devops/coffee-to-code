cdc96cd64f4f209361b4ae95c8c6685494e17695
Nick Jones <nick.jones@FVFXKBUNHV27.local>
2019-04-15 16:31:50 +0900

    cleaning up

--- public/client.js
+++ public/client.js
@@ -1,8 +1,3 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-console.log('hello world :o');
-
 // our default array of dreams
 const dreams = [
   'Find and count some sheep',
