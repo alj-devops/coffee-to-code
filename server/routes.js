87567303cab9b8d1884eb91ac9ad9d6a162b68db
Nick Jones <nick.jones@FVFXKBUNHV27.local>
2019-05-09 15:43:41 +0900

    basic structure

--- server/routes.js
+++ server/routes.js
@@ -1,25 +1,30 @@
-const sharedPageData = require('../page-data/shared.json');
-const homePageData = require('../page-data/home.json');
-const aboutPageData = require('../page-data/about-coffee.json');
+const sharedData = require('../page-data/shared.json');
+const homeData = require('../page-data/home.json');
+const aboutData = require('../page-data/about-coffee.json');
 const coffeeTypesPageData = require('../page-data/coffee-types.json');
+const whyCoffeeData = require('../page-data/why-coffee.json');
 
 module.exports = (app) => {
   app.use(require('./view-data'));
 
   app.get('/', (req, res) => {
-    res.render('pages/home.html', { shared: sharedPageData, page: homePageData });
+    res.render('pages/home.html', { shared: sharedData, page: homeData });
   });
 
   app.get('/about-coffee', (req, res) => {
-    res.render('pages/about-coffee.html', { shared: sharedPageData, page: aboutPageData });
+    res.render('pages/standard-template.html', { shared: sharedData, page: aboutData });
   });
 
   app.get('/coffee-types', (req, res) => {
-    res.render('pages/coffee-types.html', { shared: sharedPageData, page: coffeeTypesPageData });
+    res.render('pages/coffee-types.html', { shared: sharedData, page: coffeeTypesPageData });
+  });
+
+  app.get('/why-coffee', (req, res) => {
+    res.render('pages/standard-template.html', { shared: sharedData, page: whyCoffeeData });
   });
 
   app.get('/*', (req, res) => {
-    res.render('pages/error.html', { shared: sharedPageData });
+    res.render('pages/error.html', { shared: sharedData });
   });
 
   return app;
